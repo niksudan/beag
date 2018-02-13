70fa9a36d94d42e50105e01085a850f7854ab176
Nik Sudan <nik@forge.uk>
2018-02-13 20:56:28 +0000

    Able to log into Discord

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('🐶');
+import { Client } from 'discord.js';
+
+require('dotenv').config();
+
+const client = new Client();
+
+client.on('ready', () => {
+  console.log('🐶 Beag Online');
+});
+
+client.login(process.env.TOKEN);
